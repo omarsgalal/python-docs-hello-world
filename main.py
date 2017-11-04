eab98a3582f1c904ade401a9d45981fe9ecab7d3
omarsgalal <omarsgalal4@gmail.com>
2017-11-04 17:43:41 +0200

    hello Omar

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 def hello_world():
   return 'Hello, World!'
 
-@app.route('/hello/<name>')
-def hello(name):
-	return 'Hello {}'.format(name)
+@app.route('/hello')
+def hello():
+	return 'Hello Omar'
 
 if __name__ == '__main__':
   app.run()
