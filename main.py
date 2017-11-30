7b8d65041c993849ef87c63155c5177c012951d7
omarsgalal <omarsgalal4@gmail.com>
2017-11-30 22:11:40 +0200

    try pypyodbc

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
+import pypyodbc
 
 @app.route('/')
 def student():
-   return render_template('student.html')
+   #return render_template('student.html')
+   conn = pypyodbc.connect(Trusted_Connection='yes', driver = '{SQL Server}',server = 'LAPTOP-HG7BB9JM' , database = 'WareHubDB')
+   return 'omar'
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
